978def7a0f01c0da0fc4c2da30943b7656e61b8d
xiaofengXia <1066433400@qq.com>
2016-04-14 17:58:31 +0800

    first commit

---
+++ IndianaTests/IndianaTests.m
@@ -0,0 +1,39 @@
+//
+//  IndianaTests.m
+//  IndianaTests
+//
+//  Created by Tagcare on 16/4/14.
+//  Copyright © 2016年 Tagcare. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IndianaTests : XCTestCase
+
+@end
+
+@implementation IndianaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
