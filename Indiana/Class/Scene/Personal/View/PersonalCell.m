4c882bac41d5b918b26f11748c110893d4b81793
Angela527 <374536021@qq.com>
2016-06-21 18:14:08 +0800

    界面

---
+++ Indiana/Class/Scene/Personal/View/PersonalCell.m
@@ -0,0 +1,24 @@
+//
+//  PersonalCell.m
+//  Indiana
+//
+//  Created by Tagcare on 16/6/21.
+//  Copyright © 2016年 Tagcare. All rights reserved.
+//
+
+#import "PersonalCell.h"
+
+@implementation PersonalCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
